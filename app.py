909258fcd3dc8cca08a2841329499d49f99dde2d
burn56 <matt.t.urbano@gmail.com>
2024-03-14 19:47:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, request, abort
 import requests
+import os
 
 app = Flask(__name__)
 
-@app.route('/play_movie')
-def play_movie():
-    plex_token = 'Your_Plex_Auth_Token'
-    player_id = 'Your_Player_Identifier'
-    movie_id = 'Your_Movie_Identifier'
+@app.route('/play_movie/<player_id>/<movie_id>')
+def play_movie(player_id, movie_id):
+    plex_token = os.getenv('PLEX_TOKEN')
+    if not plex_token:
+        abort(500, "Plex token is not set in the environment variables.")
     plex_url = f'http://your-plex-server-ip:32400/player/playback/playMedia?X-Plex-Token={plex_token}&key=/library/metadata/{movie_id}&machineIdentifier={player_id}'
     response = requests.get(plex_url)
     return 'Playing movie'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=4942)
